20b1924cffcf1a7910cbc59eaac44cb8be34ed38
Hyunho Kim <69750502+kimho912@users.noreply.github.com>
2022-10-30 01:22:25 -0500

    full_creadit for P08

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <iostream>
+
+int main() {
+    std::string name;
+    std::cout << "Whar's your name? ";
+    std::getline(std::cin, name);
+    std::cout << "Hello, " << name << "!" << std::endl;
+}
